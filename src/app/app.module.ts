e9921d98661492cff623104128931df04b204893
Vitor Barbosa <vitor_db@outlook.com>
2022-10-05 18:02:43 -0300

    first changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
+import { CrudComponent } from './pages/crud/crud.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { IConfig, NgxMaskModule } from 'ngx-mask';
+
+registerLocaleData(localePt, 'pt');
+
+const maskConfig: Partial<IConfig> = {
+  validation: false,
+};
+
+@NgModule({
+  declarations: [AppComponent, CrudComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxMaskModule.forRoot(maskConfig),
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/pages/crud/crud.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit, PipeTransform } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+export interface user {
+  userName: string;
+  phone: number;
+  actions: any;
+}
+@Component({
+  selector: 'app-crud',
+  templateUrl: './crud.component.html',
+  styleUrls: ['./crud.component.scss'],
+})
+export class CrudComponent implements OnInit {
+  columnsToDisplay = ['userName', 'phone', 'actions'];
+  newUser: FormGroup;
+  filters: FormGroup;
+
+  table: user[] = [];
+
+  myDataArray: any; // dataSource
+  name: string;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.newUser = this.fb.group({
+      userName: ['', Validators.required],
+      phone: ['', Validators.required],
+    });
+    this.filters = this.fb.group({
+      filter: [''],
+    });
+  }
+
+  // adiciona um usuário
+  addUser() {
+    if (this.newUser.valid) {
+      const newTable = this.table;
+      newTable.push(this.newUser.value);
+      this.myDataArray = [...newTable];
+    } else {
+      alert('Favor inserir os dados corretamente!');
+    }
+  }
+
+  // remove um usuário
+  removeUser(value: any) {
+    this.table = this.table.filter((row) => {
+      return row != value;
+    });
+    this.myDataArray = [...this.table];
+  }
+
+  // filtra por nome ou telefone
+  filterUser(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.myDataArray.filter = filterValue
+      .trim()
+      .toLocaleLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+    let index = this.myDataArray.findIndex(
+      (v: any) => v.userName === filterValue || v.phone === filterValue
+    );
+    if (filterValue) {
+      this.myDataArray = [
+        {
+          userName: this.myDataArray[index].userName,
+          phone: this.myDataArray[index].phone,
+        },
+      ];
+    } else {
+      this.myDataArray = this.table;
+    }
+  }
+}
