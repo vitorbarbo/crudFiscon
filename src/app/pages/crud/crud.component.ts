9913e68a18fdfb2af6aef5ff74a55e4aa6291212
Vitor Barbosa <vitor_db@outlook.com>
2022-10-05 21:46:37 -0300

    implementing mask and updating cli

--- src/app/pages/crud/crud.component.ts
+++ src/app/pages/crud/crud.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, PipeTransform } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export interface user {
